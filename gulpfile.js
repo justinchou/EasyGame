917294c44d719d8c1f53e39b916549e71a567ade
Justin <zhou78620051@126.com>
2018-02-24 10:21:06 +0800

    使用gulp解决mocha.opt无法配置多环境问题, 尝试编写Gulp的pipe插件

--- gulpfile.js
+++ gulpfile.js
@@ -9,17 +9,57 @@
 'use strict';
 
 const Gulp = require('gulp');
-const Mocha = require('gulp-mocha');
+const Through = require('through2');
+const Mocha = require('./zutils/modules/gulp-mocha-reporter');
 const ConfigMocha = require('./config/mocha');
 
 Gulp.task('default', function () {
     return Gulp.src(['z*/test/**/*_test.js'], {read: false})
         .pipe(Mocha({
             reporter: 'spec',
+            checkLeaks: true,
 
             timeout: ConfigMocha.timeout,
             globals: {
                 should: require('chai').should()
             }
+        }));
+});
+
+Gulp.task('jenkins', function () {
+    return Gulp.src(['z*/test/**/*_test.js'], {read: false})
+        .pipe(Mocha({
+            reporter: 'xunit',
+            reporterOptions: {output: 'ztests/mocha.xml'},
+
+            timeout: ConfigMocha.timeout,
+            globals: {
+                should: require('chai').should()
+            }
+        }));
+});
+
+Gulp.task('github', function () {
+    return Gulp.src(['z*/test/**/*_test.js'], {read: false})
+        .pipe(Mocha({
+            reporter: 'markdown',
+            result: true,
+            reporterOptions: {output: 'ztests/mocha.md', haha: 11},
+
+            timeout: ConfigMocha.timeout,
+            globals: {
+                should: require('chai').should()
+            }
+        }));
+});
+
+Gulp.task('mine', function () {
+    return Gulp.src(['z*/test/**/*_test.js'], {read: false})
+        .pipe(Through.obj(function (file, encoding, cb) {
+            console.log('=====================================');
+            console.log(file.relative);
+            console.log(file.path);
+            this.push(file);    // 必须push一下, 否则后面的pipe无法接收到文件.
+            cb();
         }));
 });
